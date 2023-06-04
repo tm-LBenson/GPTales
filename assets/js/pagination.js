c77deaee30f48d3dcb6b5fc9cd52a451f37c1724
tm-LBenson <tm.lewisbenson@gmail.com>
2023-06-03 23:13:40 -0400

    change javascript to new syntax, grab class

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,20 +1,20 @@
 console.log('pagination running...')
 window.onload = function() {
-  var contentDiv = document.getElementById('post-content');
+  let contentDiv = document.querySelector('.post-content');
   if (!contentDiv) return;
 
-  var contentHtml = contentDiv.innerHTML;
-  var pages = contentHtml.split('<!-- pagebreak -->');
+  let contentHtml = contentDiv.innerHTML;
+  let pages = contentHtml.split('<!-- pagebreak -->');
 
   if (pages.length <= 1) return;
 
-  var currentPage = 0;
+  let currentPage = 0;
   function showPage(pageNumber) {
     contentDiv.innerHTML = pages[pageNumber];
     currentPage = pageNumber;
   }
 
-  var nextButton = document.createElement('button');
+  let nextButton = document.createElement('button');
   nextButton.textContent = 'Next Page';
   nextButton.onclick = function() {
     if (currentPage < pages.length - 1) {
@@ -22,7 +22,7 @@ window.onload = function() {
     }
   };
 
-  var prevButton = document.createElement('button');
+  let prevButton = document.createElement('button');
   prevButton.textContent = 'Previous Page';
   prevButton.onclick = function() {
     if (currentPage > 0) {
