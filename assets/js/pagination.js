09e2f0134979d445eb0e89ec3222774dd0713ccc
tm-LBenson <tm.lewisbenson@gmail.com>
2023-06-03 23:53:38 -0400

    adjust pagination

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -9,25 +9,61 @@ window.onload = function () {
   if (pages.length <= 1) return;
 
   let currentPage = 0;
+
+  function createPageButton(pageIndex) {
+    let button = document.createElement('button');
+    button.textContent = pageIndex + 1;
+    button.className = 'pagination-button page-number-button';
+    button.onclick = function () {
+      showPage(pageIndex);
+    };
+    return button;
+  }
+
+  function createEllipsisButton() {
+    let button = document.createElement('button');
+    button.textContent = '...';
+    button.className = 'pagination-button page-number-button';
+    button.disabled = true;
+    return button;
+  }
+
+  function updatePageButtons(pageNumber) {
+    pageNumberButtons.forEach((button) => button.remove());
+
+    pageNumberButtons = [];
+
+    pageNumberButtons.push(createPageButton(0)); // First page
+    if (pageNumber > 3) pageNumberButtons.push(createEllipsisButton());
+
+    let start = Math.max(1, pageNumber - 1);
+    let end = Math.min(pageNumber + 2, pages.length - 1);
+    for (let i = start; i < end; i++) {
+      pageNumberButtons.push(createPageButton(i));
+    }
+
+    if (pageNumber < pages.length - 4)
+      pageNumberButtons.push(createEllipsisButton());
+    pageNumberButtons.push(createPageButton(pages.length - 1)); // Last page
+
+    pageNumberButtons.forEach((button) =>
+      paginationContainer.insertBefore(button, nextButton),
+    );
+  }
+
   function showPage(pageNumber) {
     contentDiv.innerHTML = pages[pageNumber];
     currentPage = pageNumber;
 
     pageNumberButtons.forEach(function (btn, index) {
-      btn.disabled = index === pageNumber;
+      btn.disabled = btn.textContent === (currentPage + 1).toString();
     });
 
-    if (currentPage === 0) {
-      prevButton.style.display = 'none';
-    } else {
-      prevButton.style.display = 'inline';
-    }
+    prevButton.style.display = currentPage === 0 ? 'none' : 'inline';
+    nextButton.style.display =
+      currentPage === pages.length - 1 ? 'none' : 'inline';
 
-    if (currentPage === pages.length - 1) {
-      nextButton.style.display = 'none';
-    } else {
-      nextButton.style.display = 'inline';
-    }
+    updatePageButtons(pageNumber);
   }
 
   let nextButton = document.createElement('button');
@@ -48,23 +84,12 @@ window.onload = function () {
     }
   };
 
-  let pageNumberButtons = pages.map(function (_, index) {
-    let button = document.createElement('button');
-    button.textContent = index + 1;
-    button.className = 'pagination-button page-number-button';
-    button.onclick = function () {
-      showPage(index);
-    };
-    return button;
-  });
+  let pageNumberButtons = [];
 
   let paginationContainer = document.createElement('div');
   paginationContainer.className = 'pagination-container';
 
   paginationContainer.append(prevButton);
-  pageNumberButtons.forEach(function (btn) {
-    paginationContainer.append(btn);
-  });
   paginationContainer.append(nextButton);
 
   contentDiv.after(paginationContainer);
